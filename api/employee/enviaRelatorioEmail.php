a6d8b883f175b1cba7387d29503f49a445d4414d
TradeDev2 <116595308+TradeDev2@users.noreply.github.com>
2023-05-30 09:31:36 -0300

    Alterada função de envio de relatorio por email

--- api/employee/enviaRelatorioEmail.php
+++ api/employee/enviaRelatorioEmail.php
@@ -23,51 +23,54 @@ $return = ['successes' => [], 'failures' => [], 'warnings' => []];
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer;
-$mail->CharSet="UTF-8";
+$mail->CharSet = "UTF-8";
 
-for ($i=0; $i<count($emails); $i++) {
-try {
-    //Server settings
-    //$mail->SMTPDebug = SMTP::DEBUG_CONNECTION;                      //Enable verbose debug output
-    $mail->isSMTP();       //Send using SMTP
-    $mail->SMTPOptions = [
-        'ssl' => [
-          'verify_peer' => false,
-          'verify_peer_name' => false,
-          'allow_self_signed' => true
-        ]
-      ];
+if ($email[0]) {
+    try {
+        //Server settings
+        //$mail->SMTPDebug = SMTP::DEBUG_CONNECTION;                      //Enable verbose debug output
+        $mail->isSMTP();       //Send using SMTP
+        $mail->SMTPOptions = [
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            ]
+        ];
 
-    $mail->Host       = 'smtp.tradesystem.com.br';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'no-reply@tradesystem.com.br';                     //SMTP username
-    $mail->Password   = 'Trade@6760@no-reply';                               //SMTP password
-    //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port  = 587;   
+        $mail->Host       = 'smtp.tradesystem.com.br';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'no-reply@tradesystem.com.br';                     //SMTP username
+        $mail->Password   = 'Trade@6760@no-reply';                               //SMTP password
+        //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port  = 587;
 
-    //Recipients
-    $mail->setFrom('no-reply@tradesystem.com.br', 'Sultrade');
+        //Recipients
+        $mail->setFrom('no-reply@tradesystem.com.br', 'Sultrade');
 
-        $mail->addAddress($emails[$i]);     //Add a recipient
+        foreach ($emails as $email) {
+            $mail->addAddress($email);     //Add a recipient
+
+            if (!PHPMailer::validateAddress($email)) {
+                array_push($return['failures'], $email);
+            } else {
+                array_push($return['successes'], $email);
+            }
+        }
+        //$mail->addAddress('disbursements@sultradeagency.com');
         $mail->addCC('no-reply@tradesystem.com.br');
         $mail->addBCC('no-reply@tradesystem.com.br');
-        
+
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
         $mail->Subject = 'SOA - Statement of Account';
         $mail->Body    = $mensagem;
+
         
-        if (!PHPMailer::validateAddress($emails[$i])){ 
-            array_push($return['failures'], $emails[$i]);
-         } else {
-            $mail->send();
-            array_push($return['successes'], $emails[$i]);
-        }
     } catch (Exception $e) {
         array_push($return['warnings'], "Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
     }
 }
 
-echo(json_encode($return));
+echo (json_encode($return));
 exit;
-?>
