85a361d9d95daff2e0c9c38a2d0606e5b69a42af
tradesystem <dev@tradesystem.com.br>
2023-11-07 19:36:04 -0300

    add external route api in axios

--- src/services/apiamrg.js
+++ src/services/apiamrg.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { CAMINHO_API, CAMINHO_API_EXTERNO } from '../config'
+import { CAMINHO_API } from '../config'
 
 var url = '187.86.143.231'
 if (document.URL.search('10.0.0.220') >= 0) {
@@ -14,7 +14,7 @@ var apiClient = axios.create({
 
 var apiEmployee = axios.create({
     //baseURL: 'http://10.0.0.220:8080/html/site/api/employee/',
-    baseURL: `${document.URL.search('192.168.59.232') >= 0 ? CAMINHO_API : CAMINHO_API_EXTERNO }`,
+    baseURL: `${CAMINHO_API}`,
     timeout: 30000
 })
 
