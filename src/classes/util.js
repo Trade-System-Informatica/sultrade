94c89874bfdbe88b8a08f57362ef7a3fa74d15bc
TradeDev2 <dev2@tradesystem.com.br>
2024-01-22 09:46:45 -0300

    feat: verificando data das taxas e validando com a data de encerramento da os, para ver se aplica ou não a taxa.

--- src/classes/util.js
+++ src/classes/util.js
@@ -8,6 +8,27 @@ export default class Util {
         const resto = length - String(str).length
         return '0'.repeat(resto > 0 ? resto : '0') + str
     }
+    static verificaDatas(data1, data2){
+        const date1 = moment(data1, "yyyy-MM-DD").format("YYYYMMDD");
+        const date2 = moment(data2, "yyyy-MM-DD").format("YYYYMMDD");
+        console.log(date1, date2)
+        if (date2 == 'Invalid date'){
+            console.log('data invalida')
+            return false
+        }else{
+            console.log('passou na primeira validação de data')
+            if (data2[4]== '-' && data2[7] == '-'){
+                console.log('passou na validacao dos hifens')
+                if (date2 > date1) {
+                    console.log('aplica')
+                    return true;
+                }else{
+                    console.log('nao aplica')
+                    return false;
+                }
+            }
+        }
+    }
 
     static formataData(data) {
         return moment(data).format('yyyy-MM-DD')
