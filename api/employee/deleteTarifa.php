31a29dc08909e5d747836f034b369a8edadcd5a2
TradeDev2 <dev2@tradesystem.com.br>
2024-01-24 11:38:00 -0300

    fix: ajustando delete de tarifa, agora deleta os anexos juntos, e ajustando insert pra deletar anexos caso já existam na inserção.

--- api/employee/deleteTarifa.php
+++ api/employee/deleteTarifa.php
@@ -12,6 +12,11 @@ $objData = json_decode($data);
 if($objData != NULL){
     $token = prepareInput($objData->token);
     $chave = prepareInput($objData->chave);
+    $anexo1 = prepareInput($objData->anexo1);
+    $anexo2 = prepareInput($objData->anexo2);
+
+    deletePictureTarifa($anexo1);
+    deletePictureTarifa($anexo2);
 
     $pessoas = new Pessoas();
     
