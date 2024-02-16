40693fafb8164c84f5fc378315f7090a792976b9
TradeDev2 <dev2@tradesystem.com.br>
2024-02-16 07:33:38 -0300

    fix merge branchDev

--- src/screens/ordensservico/addevento/index.js
+++ src/screens/ordensservico/addevento/index.js
@@ -340,7 +340,8 @@ class AddEvento extends Component {
     getTemplates = async () => {
         await apiEmployee.post(`getEventosTemplates.php`, {
             token: true,
-            empresa: this.state.usuarioLogado.empresa
+            empresa: this.state.usuarioLogado.empresa,
+            offset: null
         }).then(
             async res => {
                 await this.setState({ templates: res.data })
@@ -387,7 +388,10 @@ class AddEvento extends Component {
 
     getOS = async () => {
         await apiEmployee.post(`getOS.php`, {
-            token: true
+            token: true,
+            empresa: 0, 
+            limit: 'n',
+            offset: 'n'
         }).then(
             async res => {
                 await this.setState({ todasOs: res.data })
