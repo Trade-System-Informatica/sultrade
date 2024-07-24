182b5577c6b9069c76d91c913d239bc40f7b4bda
tradesystem <dev@tradesystem.com.br>
2024-07-24 12:34:47 -0300

    teste email

--- api/employee/enviaRelatorioEmail.php
+++ api/employee/enviaRelatorioEmail.php
@@ -66,7 +66,7 @@ if ($emails[0]) {
         //$mail->SMTPDebug = SMTP::DEBUG_CONNECTION;                      //Enable verbose debug output
         $mail->isSMTP();       //Send using SMTP
         $mail->SMTPOptions = [
-            'ssl' => [
+            'tsl' => [
                 'verify_peer' => false,
                 'verify_peer_name' => false,
                 'allow_self_signed' => true
@@ -74,12 +74,12 @@ if ($emails[0]) {
         ];
     
 
-        $mail->Host       = 'mail.vetorial.com'; //'Smtp.office365.com';                                               //Set the SMTP server to send through
+        $mail->Host       = 'smtp.office365.com';                   //'mail.vetorial.com';                                               //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'soa@sultradeagency.com';                     //SMTP username
-        $mail->Password   = 'Trade@2023#';                               //SMTP password
-        $mail->SMTPSecure = 'ssl'; //PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port  = 465;
+        $mail->Password   = 'C&773531409775un';                               //SMTP password
+        $mail->SMTPSecure = 'tsl'; //PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port  = 587;
 
         //Recipients
         $mail->setFrom('soa@sultradeagency.com', 'Sultrade Agency');
