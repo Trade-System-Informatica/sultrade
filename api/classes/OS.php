0b7bee4fa31785cfce9e2c90976260ce52d28917
Dev Cortez <dev@tradesystem.com.br>
2024-11-26 10:25:34 -0300

    fix

--- api/classes/OS.php
+++ api/classes/OS.php
@@ -1482,13 +1482,16 @@ class OS
 
         $query = "chave_os = " . $chave_os;
         $eventosAtuais = $database->doSelect('os_servicos_itens', "os_servicos_itens.*", "chave_os =  . $chave_os AND cancelada = 0");
-        $ordem_nova = $eventosAtuais ? max(array_column($eventosAtuais, 'ordem')) + 1 : 1;
-
+        if($eventosAtuais != NULL){
+            $ordem_nova = end($eventosAtuais)['ordem'] + 1;
+        }else{
+            $ordem_nova = 1;
+        }
         $eventos = $database->doSelect("os_servicos_itens", "os_servicos_itens.*", "chave_os = $chave_orcamento");
         
         foreach ($eventos as $evento) {
             $database->doUpdate("os_servicos_itens", $query.', ordem = '.$ordem_nova, "chave = ". $evento["chave"]);
-            $ordem_nova++;
+            $ordem_nova = $ordem_nova + 1;
         }
 
         $result = $database->doSelect('os_servicos_itens', "os_servicos_itens.*", 'chave_os = ' . $chave_os);
